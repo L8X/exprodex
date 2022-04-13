f215f685c54732dbede06676d55c220db0d2f6f4
Madi <49875377+L8X@users.noreply.github.com>
2022-04-13 03:36:06 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -2,57 +2,6 @@ loadstring(game:HttpGet("https://pastebin.com/raw/NWK2hFxB"))()
 
 Services = setmetatable({},{__index=function(s,r) return game:service(r) end})
 
-if syn and hookmetamethod and getrawmetatable then 
-spawn(function()
-local OldIndex
-OldIndex = hookmetamethod(game, "__index", function(Self, Index)
-    return OldIndex(Self, Index)
-end)
-
-local OldNewIndex
-OldNewIndex = hookmetamethod(game, "__newindex", function(Self, Index, Value)
-    return OldNewIndex(Self, Index, Value)
-end)
-
-local OldNamecall
-OldNamecall = hookmetamethod(game, "__namecall", function(Self, ...)
-    return OldNamecall(Self, ...)
-end)
-
-local mt = getrawmetatable(game)
-
-local old
-old = hookfunction(mt.__namecall, function(...)
-   return old(...)
-end)
-
-local function get_proxy_game_metatable(x)
- local proxy = {}
-  local mt_func_list = {
-    
- }
- local proxy_mt = {
-    __newindex = function(t,k,v)
-       local old
-       old = hookmetamethod(game,k,v)
-       mt_func_list[k] = old
-     end;
-      __index = function(t,k)
-         return function(...)
-             local list_func =  mt_func_list[k]
-              local unpack_pcall = {pcall(list_func,...)}
-           if unpack_pcall[1] == true then
-                 table.remove(unpack_pcall,1)
-              return unpack(unpack_pcall)
-                end
-          end
-        end
-  }
- return setmetatable(proxy,proxy_mt)
-end
-end)
-end
-
 local ScriptContext = Services.ScriptContext
 local InsertService = Services.InsertService
 local CoreGui = Services.CoreGui
