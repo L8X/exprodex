335f3b2aea0ef082fcfda902b7046805bd034f83
Madi <49875377+L8X@users.noreply.github.com>
2022-04-13 03:40:46 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,11 +1,13 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/NWK2hFxB"))()
-
 Services = setmetatable({},{__index=function(s,r) return game:service(r) end})
 
-local ScriptContext = Services.ScriptContext
-local InsertService = Services.InsertService
-local CoreGui = Services.CoreGui
-local ContentProvider = Services.ContentProvider
+local cloneref = cloneref or function(ref)
+    return ref
+end
+
+local ScriptContext = cloneref(Services.ScriptContext)
+local InsertService = cloneref(Services.InsertService)
+local CoreGui = cloneref(Services.CoreGui)
+local ContentProvider = cloneref(Services.ContentProvider)
 
 local table_insert = table.insert
 local table_foreach = table.foreach
@@ -21,24 +23,6 @@ local getobjects = function(a)
     return Objects
 end
 
-if hookfunction and getrawmetatable then
-spawn(function()
-local function old()
-hookfunction(game.GetObjects, getobjects)
-end
-
-pcall(function()
-old()
-end)
-end)
-end
-
-local contentId = ("rbxassetid://6996343412")[1]
-
-local assets = { contentId }
-
-ContentProvider:Preload("rbxassetid://6996343412")
-
-spawn(function()
-ContentProvider:PreloadAsync(assets, loadstring(getobjects('rbxassetid://6996343412')[1].Source)())
+task.spawn(function()
+loadstring(getobjects('rbxassetid://6996343412')[1].Source)()
 end)
