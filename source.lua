990682882f6824d78e924225053462aaedc5b340
Madi <49875377+L8X@users.noreply.github.com>
2022-06-07 00:42:39 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,35 +1,3 @@
-pcall(function()
-if identifyexecutor() == "ScriptWare" then
-local function decomp(a)
-    return tostring(disassemble(a))
-end
-
-getgenv().decompile = decomp
-end
-end)
-
-
-local cloneref = cloneref or function(ref)
-    return ref
-end
-
-local CoreGui = cloneref(game:GetService("CoreGui"))
-local InsertService = cloneref(game:GetService("InsertService"))
-
-local table_insert = table.insert
-local table_foreach = table.foreach
-local string_char = string.char
-local getobjects = function(a)
-    local Objects = {}
-    if a then
-        local b = InsertService:LoadLocalAsset(a)
-        if b then 
-            table_insert(Objects, b) 
-        end
-    end
-    return Objects
-end
-
 task.spawn(function()
-loadstring(getobjects('rbxassetid://6996343412')[1].Source)()
+loadstring(game:GetObjects('rbxassetid://6996343412')[1].Source)()
 end)
